1048edeb31607f0d3d3d24c6d7f1882f1a2789d6
Gabriel Hipolito <c.gabrielhipolitoh@gmail.com>
2023-04-15 13:49:29 -0300

    finalizando

--- src/Componentes/NavMenu.jsx
+++ src/Componentes/NavMenu.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
 import { Outlet,Link } from 'react-router-dom'
 import '../Styles/navmenu.css'
+import Shop from '../assets/Shop'
+import Car from '../assets/car'
+import Arow from '../assets/Arow'
+import Box from '../assets/Box'
 const NavMenu = () => {
 
     const paginas = [{
         nome: "Cadastrar produto",
-        para:"/cadastrarproduto"
+        para:"/cadastrarproduto",
+        img: <Shop/>
     },
     {
          nome: "Frente de caixa",
-         para:"/frentecaixa"
+         para:"/frentecaixa",
+         img: <Car/>
     },
     {
         nome: "Entrada e saida",
-        para:"/cadastrarproduto"
+        para:"/cadastrarproduto",
+        img:<Arow/>
     },
     {
         nome: "Fornecedores",
-        para:"/cadastrarproduto"
+        para:"/cadastrarproduto",
+        img:<Box/>
     },]
 
   return (
@@ -26,10 +34,14 @@ const NavMenu = () => {
             <h1>Pdv Nuvem</h1>
             <div className="links">
                 {paginas.map((item) => (
-                <Link to={item.para}>{item.nome}</Link>
+                <Link to={item.para}>
+                    <div className="hover">
+                        <span>{item.img}</span>
+                        <span>{item.nome}</span>
+                    </div>
+                    </Link>
                 ))}
             </div>
-          
         </nav>
         <Outlet/>
     </main>
--- src/Rotas/CadastrarProdutos.jsx
+++ src/Rotas/CadastrarProdutos.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import '../Styles/CadastrarProdutos.css'
 const CadastrarProdutos = () => {
+
+
+  const listaTable = ["Nome","Codigo INT","Codigo de barras",
+                "Fornecedor","Valor da compra","Valor da venda","Quantidade"]
+
   return (
-    <div>CadastrarProdutos</div>
+    <section>
+      <h1>Cadastrar produtos</h1>
+      <div className="boxProdutos">
+        <div className="inputs">
+          <input type="text" name="search" placeholder='Pesquisar' id="search"/>
+          <input type="button" value="Cadastrar" />
+        </div>
+        <hr/>
+        <table>
+          <tr>
+            {listaTable.map((item) =>(
+              <td>{item}</td>
+            ))}
+          </tr>
+        </table>
+      </div>
+    </section>
   )
 }
 
