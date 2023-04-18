927b7dc2b57e1b667c39521976f37d9bd36080ec
Gabriel Hipolito <c.gabrielhipolitoh@gmail.com>
2023-04-18 19:45:20 -0300

    add arquivos server,hooks

--- src/Rotas/CadastrarProdutos.jsx
+++ src/Rotas/CadastrarProdutos.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/CadastrarProdutos.css'
+import ModalCadastro from '../Modais/ModalCadastro'
+import { useFetch } from '../hooks/useFetch'
 const CadastrarProdutos = () => {
 
+  const {data} = useFetch()
+
+  const [modalCad,setModalCad] = useState(false)
 
   const listaTable = ["Nome","Codigo INT","Codigo de barras",
                 "Fornecedor","Valor da compra","Valor da venda","Quantidade"]
-
+  console.log(data)
   return (
     <section>
       <h1>Cadastrar produtos</h1>
       <div className="boxProdutos">
         <div className="inputs">
           <input type="text" name="search" placeholder='Pesquisar' id="search"/>
-          <input type="button" value="Cadastrar" />
+          <input type="button" value="Cadastrar" onClick={() => {
+            setModalCad(true)
+          }}/>
         </div>
         <hr/>
         <table>
           <tr>
-            {listaTable.map((item) =>(
-              <td>{item}</td>
+            {listaTable.map((item,index) =>(
+              <td key={index}>{item}</td>
             ))}
           </tr>
         </table>
+          {modalCad && <ModalCadastro/>}
       </div>
     </section>
   )
